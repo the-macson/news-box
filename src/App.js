586c559031c25ac3d2f90feb190baeb4ed471b7a
the-macson <ak9991927952@gmail.com>
2021-08-28 21:59:38 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from './Components/NavBar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is class based components</h1>
+        <Navbar/>
       </div>
     )
   }
