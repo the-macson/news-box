759a620940150538c6bdef400844f80280240129
the-macson <ak9991927952@gmail.com>
2021-08-30 21:22:08 +0530

    added some code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from './Components/NavBar'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <News/>
       </div>
     )
   }
